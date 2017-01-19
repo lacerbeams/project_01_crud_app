ea90d72fd31012574a8fa4eaad95dad005474405
Lacey Madison <laceyannmadison@gmail.com>
2017-01-19 14:26:07 -0800

    refactored but has bug with duplicate entries

--- public/main.js
+++ public/main.js
@@ -2,17 +2,14 @@ console.log('hello from main.js')
 var $add = $('#add');
 var $submit = $('.submit');
 
-
-
-// adds data to db
-$submit.click(function(evt) {
-  $.post('/insert', $('#form').serialize());
+function renderTable(evt) {
+  $('input').val('');
   $.get('/data', function(items) {
       var chunkOfHtml = $("#projects-template").html();
       var template = Handlebars.compile(chunkOfHtml);
       var html = template({items: items});
-      $(".details").append(html);
-      $('.delete').on('click', function(evt){
+      $('.details').append(html);
+      $('.delete').click(function(evt){
         // get the id from the button that we clicked on
         var button = $(this);
         var id = button.attr('id');
@@ -21,10 +18,20 @@ $submit.click(function(evt) {
         $.post('/projects/' + id + '/delete', function(res) {
           // remove whole row
           tr.remove();
-        })
-      })
-  })
-})
+        });
+      });
+      $('.update').click(function(evt) {
+        var button = $(this);
+        var id = button.attr('id');
+        console.log(id);
+      });
+  });
+}
 
+$submit.click(function(evt) {
+  $.post('/insert', $('#form').serialize());
+  renderTable();
+});
 
+$(document).ready(renderTable);
 
