ad48867404912e1917c72f52f2fd1e7b43f9cc22
Lacey Madison <laceyannmadison@gmail.com>
2017-03-11 11:33:17 -0800

    now able to update db

--- public/main.js
+++ public/main.js
@@ -21,21 +21,34 @@ function renderTable(evt) {
       });
        $('.update').click(function(evt) {
         console.log('clicked')
-         var button = $(this);
+         var button = $(evt.target);
          var id = button.attr('id');
          console.log(id);
          var projectRow = button.parent().closest('#' + id);
-         console.log(projectRow.children())
-         $(projectRow.children()).eq(0).html(`<input style="width:75%" class="project-input"></input>`)
-         $(projectRow.children()).eq(1).html(`<input style="width:75%" class="deadline-input"></input>`)
-         $(projectRow.children()).eq(2).html(`<input style="width:75%" class="materials-input"></input>`)
-         $(projectRow.children()).eq(3).html(`<input style="width:75%" class="notes-input"></input>`)
+         console.log(projectRow)
+         // $(projectRow.children()).eq(0).html(`<input type="text" style="width:75%" class="project-input">`)
+         // $(projectRow.children()).eq(1).html(`<input type="text" style="width:75%" class="deadline-input">`)
+         // $(projectRow.children()).eq(2).html(`<input type="text" style="width:75%" class="materials-input">`)
+         // $(projectRow.children()).eq(3).html(`<input type="text" style="width:75%" class="notes-input">`)
+         var projectInput = $(projectRow.children()).eq(0);
+         var deadlineInput = $(projectRow.children()).eq(1);
+         var materialsInput = $(projectRow.children()).eq(2);
+         var notesInput = $(projectRow.children()).eq(3);
+         projectInput.attr('contenteditable', 'true').css('background', 'white');
+         deadlineInput.attr('contenteditable', 'true').css('background', 'white');
+         materialsInput.attr('contenteditable', 'true').css('background', 'white');
+         notesInput.attr('contenteditable', 'true').css('background', 'white');
          button.text('Submit');
          button.click(function(event) {
-          updateProject = $('.project-input').val();
-          updateDeadline = $('.deadline-input').val();
-          updateMaterials = $('.materials-input').val();
-          updateNotes = $('.notes-input').val();
+          event.preventDefault()
+          // updateProject = $('.project-input').val();
+          // updateDeadline = $('.deadline-input').val();
+          // updateMaterials = $('.materials-input').val();
+          // updateNotes = $('.notes-input').val();
+          var updateProject = projectInput.text();
+          var updateDeadline = deadlineInput.text();
+          var updateMaterials = materialsInput.text();
+          var updateNotes = notesInput.text();
           // var updateData = `project=${updateProject}&deadline=${updateDeadline}&materials=${updateMaterials}&notes=${updateNotes}`
           updateData = {
             project:updateProject,
@@ -45,7 +58,7 @@ function renderTable(evt) {
           }
           console.log(updateData)
          $.post(`/projects/${id}/update`, updateData);
-         // location.reload();
+         location.reload();
          });
        });
    });
